dcea1e7efd212dc14a8830088dc2dffa420c487d
Bruno Dantas <brunoodantas7@gmail.com>
2021-07-13 23:35:31 -0300

    Introducao a diretivas de componetnes , estruturais e atributos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro-angular';
+  nome= 'Bruno';
+  adicionado = false;
+  listaFuncionarios = [];
+
+  adicionarPessoa(){
+    // console.log('Salvando'+ this.name);
+    console.log(`Salvando ${this.nome}`);
+    this.adicionado = true;
+    // this.listaFuncionarios.push(this.nome)
+  }
+
+  // alterarNome(event: any ){
+  //   // console.log(event)
+  //   this.name = event.target.value;
+  // }
 }
